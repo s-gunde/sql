f2a08dfc8e6e9b423e54a476cbe2d6a911f9bd1e
s-gunde <srujanasgit@gmail.com>
2024-09-22 00:46:45 -0400

    all questions answered

--- 02_activities/homework/homework_5_output.sql
+++ 02_activities/homework/homework_5_output.sql
@@ -38,3 +38,155 @@ GROUP BY vendor.vendor_name, product.product_name
 
 
 
+-- INSERT
+/*1.  Create a new table "product_units". 
+This table will contain only products where the `product_qty_type = 'unit'`. 
+It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
+Name the timestamp column `snapshot_timestamp`. */
+-- create a new table product_units
+CREATE TABLE product_units AS
+SELECT * 
+FROM product
+WHERE product_qty_type = 'unit'
+
+-- add new column snapshot_timestamp
+ALTER TABLE product_units
+ADD COLUMN snapshot_timestamp TIMESTAMP
+
+--input new column value as "CURRENT_TIMESTAMP"
+UPDATE product_units
+SET snapshot_timestamp = CURRENT_TIMESTAMP
+
+--display data from newly created table
+SELECT * FROM product_units
+
+product_id	product_name	product_size	product_category_id	product_qty_type	snapshot_timestamp
+3	Poblano Peppers - Organic	large	1	unit	2024-09-22 03:53:55
+4	Banana Peppers - Jar	8 oz	3	unit	2024-09-22 03:53:55
+5	Whole Wheat Bread	1.5 lbs	3	unit	2024-09-22 03:53:55
+6	Cut Zinnias Bouquet	medium	5	unit	2024-09-22 03:53:55
+7	Apple Pie	10"	3	unit	2024-09-22 03:53:55
+10	Eggs	1 dozen	6	unit	2024-09-22 03:53:55
+12	Baby Salad Lettuce Mix - Bag	1/2 lb	1	unit	2024-09-22 03:53:55
+16	Sweet Corn	Ear	1	unit	2024-09-22 03:53:55
+18	Carrots - Organic	bunch	1	unit	2024-09-22 03:53:55
+19	Farmer's Market Resuable Shopping Bag	medium	7	unit	2024-09-22 03:53:55
+20	Homemade Beeswax Candles	6"	7	unit	2024-09-22 03:53:55
+21	Organic Cherry Tomatoes	pint	1	unit	2024-09-22 03:53:55
+23	Maple Syrup - Jar	8 oz	2	unit	2024-09-22 03:53:55
+8	Cherry Pie	10"	3	unit	2024-09-22 03:53:55
+
+Execution finished without errors.
+Result: 14 rows returned in 13ms
+At line 12:
+SELECT * FROM product_units
+
+/*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 	
+This can be any product you desire (e.g. add another record for Apple Pie). */
+INSERT INTO product_units (product_id, product_name, product_size, product_category_id, product_qty_type, snapshot_timestamp)
+VALUES (99, 'Strawberry Pie', '10"', 3, 'unit', CURRENT_TIMESTAMP)
+
+SELECT * FROM product_units
+
+product_id	product_name	product_size	product_category_id	product_qty_type	snapshot_timestamp
+3	Poblano Peppers - Organic	large	1	unit	2024-09-22 03:53:55
+4	Banana Peppers - Jar	8 oz	3	unit	2024-09-22 03:53:55
+5	Whole Wheat Bread	1.5 lbs	3	unit	2024-09-22 03:53:55
+6	Cut Zinnias Bouquet	medium	5	unit	2024-09-22 03:53:55
+7	Apple Pie	10"	3	unit	2024-09-22 03:53:55
+10	Eggs	1 dozen	6	unit	2024-09-22 03:53:55
+12	Baby Salad Lettuce Mix - Bag	1/2 lb	1	unit	2024-09-22 03:53:55
+16	Sweet Corn	Ear	1	unit	2024-09-22 03:53:55
+18	Carrots - Organic	bunch	1	unit	2024-09-22 03:53:55
+19	Farmer's Market Resuable Shopping Bag	medium	7	unit	2024-09-22 03:53:55
+20	Homemade Beeswax Candles	6"	7	unit	2024-09-22 03:53:55
+21	Organic Cherry Tomatoes	pint	1	unit	2024-09-22 03:53:55
+23	Maple Syrup - Jar	8 oz	2	unit	2024-09-22 03:53:55
+8	Cherry Pie	10"	3	unit	2024-09-22 03:53:55
+99	Strawberry Pie	10"	3	unit	2024-09-22 04:07:44
+
+Execution finished without errors.
+Result: 15 rows returned in 21ms
+At line 15:
+--display data from newly created table
+SELECT * FROM product_units
+
+-- DELETE
+/* 1. Delete the older record for the whatever product you added. 
+
+HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
+DELETE FROM product_units
+WHERE product_id = 99
+
+Execution finished without errors.
+Result: query executed successfully. Took 0ms, 1 rows affected
+At line 21:
+DELETE FROM product_units
+WHERE product_id = 99
+
+
+
+-- UPDATE
+/* 1.We want to add the current_quantity to the product_units table. 
+First, add a new column, current_quantity to the table using the following syntax.
+
+ALTER TABLE product_units
+ADD current_quantity INT;
+
+Then, using UPDATE, change the current_quantity equal to the last quantity value from the vendor_inventory details.
+
+HINT: This one is pretty hard. 
+First, determine how to get the "last" quantity per product. 
+Second, coalesce null values to 0 (if you don't have null values, figure out how to rearrange your query so you do.) 
+Third, SET current_quantity = (...your select statement...), remembering that WHERE can only accommodate one column. 
+Finally, make sure you have a WHERE statement to update the right row, 
+	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
+When you have all of these components, you can run the update statement. */
+--display product_units table
+SELECT * 
+FROM product_units
+
+--add new column current_quantity
+ALTER TABLE product_units
+ADD current_quantity INT
+
+--create temp table to store last product quantity 
+CREATE TEMP TABLE temp_last_product_quantity AS
+SELECT product.product_id, MAX(vendor_inventory.market_date) as last_sold_on, COALESCE(vendor_inventory.quantity, 0) AS last_quantity
+FROM product
+LEFT JOIN vendor_inventory
+ON product.product_id = vendor_inventory.product_id 
+GROUP BY product.product_id
+
+SELECT * FROM temp_last_product_quantity
+
+--update last quantity for products in product_units table
+UPDATE product_units 
+SET current_quantity = (
+  SELECT last_quantity 
+  FROM temp_last_product_quantity
+  WHERE temp_last_product_quantity.product_id = product_units.product_id)
+
+product_id	product_name	product_size	product_category_id	product_qty_type	snapshot_timestamp	current_quantity
+3	Poblano Peppers - Organic	large	1	unit	2024-09-22 03:53:55	60
+4	Banana Peppers - Jar	8 oz	3	unit	2024-09-22 03:53:55	30
+5	Whole Wheat Bread	1.5 lbs	3	unit	2024-09-22 03:53:55	20
+6	Cut Zinnias Bouquet	medium	5	unit	2024-09-22 03:53:55	0
+7	Apple Pie	10"	3	unit	2024-09-22 03:53:55	10
+10	Eggs	1 dozen	6	unit	2024-09-22 03:53:55	0
+12	Baby Salad Lettuce Mix - Bag	1/2 lb	1	unit	2024-09-22 03:53:55	0
+16	Sweet Corn	Ear	1	unit	2024-09-22 03:53:55	140
+18	Carrots - Organic	bunch	1	unit	2024-09-22 03:53:55	0
+19	Farmer's Market Resuable Shopping Bag	medium	7	unit	2024-09-22 03:53:55	0
+20	Homemade Beeswax Candles	6"	7	unit	2024-09-22 03:53:55	0
+21	Organic Cherry Tomatoes	pint	1	unit	2024-09-22 03:53:55	0
+23	Maple Syrup - Jar	8 oz	2	unit	2024-09-22 03:53:55	0
+8	Cherry Pie	10"	3	unit	2024-09-22 03:53:55	10
+
+Execution finished without errors.
+Result: 14 rows returned in 21ms
+At line 1:
+--display product_units table
+SELECT * 
+FROM product_units
+
